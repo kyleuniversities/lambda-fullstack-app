6ab9d530a3410ca3c44c2bdd92d83d1cbcbc2afc
kyleuniversities <kyleuniversities@gmail.com>
2023-10-22 01:50:12 -0700

    added func class maker functionality

--- src/main/java/com/lambda/lambda/app/controller/java/JavaController.java
+++ src/main/java/com/lambda/lambda/app/controller/java/JavaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.lambda.lambda.app.helper.CodeHelper;
 import com.lambda.lambda.app.util.LambdaArguments;
+import com.lambda.lambda.app.util.java.JavaFunctionClassMaker;
 import com.lambda.lambda.app.util.java.JavaStructureClassMaker;
 import com.lambda.lambda.common.util.string.StringList;
 
@@ -18,6 +19,14 @@ public final class JavaController {
     public String structure(@RequestBody LambdaArguments lambdaArguments) {
         StringList classLines = JavaStructureClassMaker.newInstance()
                 .make(lambdaArguments.getBodyText(), lambdaArguments.argumentsContain("-setters"));
+        String classLinesText = classLines.toString();
+        return CodeHelper.toCode(classLinesText);
+    }
+
+    @PostMapping("/function")
+    public String function(@RequestBody LambdaArguments lambdaArguments) {
+        StringList classLines =
+                JavaFunctionClassMaker.newInstance().make(lambdaArguments.getBodyText());
         String classLinesText = classLines.toString();
         return CodeHelper.toCode(classLinesText);
     }
