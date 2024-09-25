8939d22ed73b003b6670ee3da2d100c0ed53f31a
kyleuniversities <kyleuniversities@gmail.com>
2024-09-24 20:16:54 -0700

    set up initial styles

--- backend/src/next/src/components/SiteUpperHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SiteUpperHeader() {
-  return (
-    <div className="w-full flex border border-blue-500 p-3">
-      <div className="title-color text-3xl font-bold">Lambda Application</div>
-    </div>
-  )
-}
