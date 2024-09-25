f4d74c63531600da6c9958fa667871d3e666ebd7
kyleuniversities <kyleuniversities@gmail.com>
2024-09-24 20:19:12 -0700

    added next lambda base components

---
+++ frontend/components/SiteConsoleCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type ConsoleCardParameters = {
+    image: string;
+    title: string;
+}
+
+export default function SiteConsoleCard(parameters: ConsoleCardParameters) {
+  return (
+    <div className="site-console-card">SiteConsoleCard</div>
+  )
+}
---
+++ frontend/components/SiteHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SiteBodyHeader from './SiteBodyHeader'
+import SiteUpperHeader from './SiteUpperHeader'
+
+export default function SiteHeader() {
+  return (
+    <>
+        <SiteUpperHeader />
+        <SiteBodyHeader />
+    </>
+  )
+}
